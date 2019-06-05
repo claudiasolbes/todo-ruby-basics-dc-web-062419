afa4b432eeb26a82e22b805dc8d4461c2e37c3c0
Claudia Solbes <cssolbes@hotmail.com>
2019-06-05 18:02:43 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,3 +1,28 @@
 def division(12, 2)
   return "#{division}"
 end
+
+
+def assign_variable("Bob")
+  return "#{assign_variable}"
+end
+
+def argue
+
+end
+
+def greeting(name)
+ return "#{greeting}"
+end
+
+def return_a_value("Nice")
+  return "#{return_a_value}"
+end
+
+def last_evaluated_value("expert")
+  return "#{last_evaluated_value}"
+end
+
+def pizza_party(topping = "cheese")
+  return "#{pizza_party}"
+end
