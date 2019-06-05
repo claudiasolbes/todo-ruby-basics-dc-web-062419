2b101de9589e94fe8429189a9fd0dcb526545dbf
Claudia Solbes <cssolbes@hotmail.com>
2019-06-05 18:34:18 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,26 +1,3 @@
-def division(num1, num2)
-  12/2
-end
-
-def assign_variable(name)
-  return "#{name}"
-end
-
-def argue(argument)
-  return "#{argument}"
-end
-
-def greeting(greeting, name)
-  return "#{greeting}"
-end
-
-def return_a_value("Nice")
-  return "#{return_a_value}"
-end
-
-def last_evaluated_value("expert")
-  return "#{last_evaluated_value}"
-end
 
 def pizza_party(topping = "cheese")
   return "#{topping}"
